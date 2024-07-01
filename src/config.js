d01181f3f27c6fa6c06116671c65aa0018ec6521
Abhishek Singh <129683164+abhi14abs@users.noreply.github.com>
2024-07-01 12:50:51 +0530

    first commit

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+// export const BASE_URL = "http://localhost:5000/api/v1";   https://car-rental-backend-e9ff.onrender.com
+export const BASE_URL = "https://car-rental-backend-e9ff.onrender.com";
 export const token = localStorage.getItem("token");
